044057ad8fff832430342c2535a641f76b123c75
iamdevsajjad <sk.sajjad4207@gmail.com>
2024-08-19 23:29:42 +0600

    movie model created

--- src/modules/movies/movie.model.ts
+++ src/modules/movies/movie.model.ts
@@ -1,18 +1,20 @@
-import { Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 import { IMovie, IReviews } from "./movie.interface";
 
 const reviewSchema = new Schema<IReviews>({
   email: { type: String, required: true },
   rating: { type: Number, required: true },
-  reviews: { type: String, required: true },
+  reviews: { type: String },
 });
 
 const movieSchema = new Schema<IMovie>({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  releaseDate: { type: String, required: true },
+  title: { type: String, required: [true, "Tittle is required."] },
+  description: { type: String, required: [true, "Description is required."] },
+  releaseDate: { type: String },
   genre: { type: String, required: true },
-  isDeleted: { type: Boolean, required: true },
-  viewCount: { type: Number, required: true },
+  isDeleted: { type: Boolean, default: false },
+  viewCount: { type: Number, default: 0 },
   reviews: [reviewSchema],
 });
+
+export const Movie = model<IMovie>("Movie", movieSchema);
